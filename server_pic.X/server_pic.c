339d9fc091648e4e737faec0dd424d703ab9e648
FrancisVijith <55200827+FrancisVijith@users.noreply.github.com>
2020-04-16 00:23:39 -0400

    Update server_pic.c

--- server_pic.X/server_pic.c
+++ server_pic.X/server_pic.c
@@ -1,161 +1,142 @@
+/* 
+ * File:   function.h
+ * Author: SURINDER  MEHTOT
+ *
+ * Created on March 18, 2020, 5:21 PM
+ */
+#include "config.h"
+//LCD pins
+#define RS RB2  //defining pins for LCD interface.
+#define EN RB3
+#define D4 RB4
+#define D5 RB5
+#define D6 RB6
+#define D7 RB7          
+//IR sensor PINS
+#define SW1 RD0 //hereby defining pins for IR sensor at entry
+#define SW2 RD1  //IR at first spot
+#define SW3 RD2  //IR at second spot
+#define SW4 RD5  //IR at third spot
+#define SW5 RD4 //IR at fourth spot
+//led pins
+#define LED1 RC5 
+#define LED2 RC6
+#define LED3 RB0
+#define LED4 RB1 
 
-#define _XTAL_FREQ 20000000
-# define SW RB1
-# define IR RB7
-#define RS RD2
-#define EN RD3
-#define D4 RD4
-#define D5 RD5
-#define D6 RD6
-#define D7 RD7
-#define Baud_rate 9600
-#include <xc.h>
-#include "pic16f877a.h"
-
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-
-
-/** Some constants **/
-
-#define ESP8266_STATION 0x01
-#define ESP8266_SOFTAP 0x02
-
-#define ESP8266_TCP 1
-#define ESP8266_UDP 0
-
-#define ESP8266_OK 1
-#define ESP8266_READY 2
-#define ESP8266_FAIL 3
-#define ESP8266_NOCHANGE 4
-#define ESP8266_LINKED 5
-#define ESP8266_UNLINK 6
-
-
-
-
-
-#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
-#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = OFF       // Power-up Timer Enable bit (PWRT enabled)
-#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
-#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
-#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
-#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
-#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
-
-void _esp8266_print(unsigned const char *);
-inline unsigned char _esp8266_waitResponse(void);
-inline uint16_t _esp8266_waitFor(unsigned char *);
-
-char apiKey = "HIGI5NSCT2TNP1GG"; //thingspeak API write key
-char ssid = "Francis";//wifi ssid 
-char password = "bananana";//wifi password
-char getStr = "GET /update?api_key="+apiKey;   // prepare GET string
-
-//****LCD Functions Developed by Circuit Digest.***///
-void Lcd_SetBit(char data_bit) //Based on the Hex value Set the Bits of the Data Lines
+#define redled RC4  //red led at entry
+void Lcd_Port(char a)
 {
-    if(data_bit& 1) 
-        D4 = 1;
-    else
-        D4 = 0;
+	if(a & 1)
+		D4 = 1;
+	else
+		D4 = 0;
 
-    if(data_bit& 2)
-        D5 = 1;
-    else
-        D5 = 0;
+	if(a & 2)
+		D5 = 1;
+	else
+		D5 = 0;
 
-    if(data_bit& 4)
-        D6 = 1;
-    else
-        D6 = 0;
+	if(a & 4)
+		D6 = 1;
+	else
+		D6 = 0;
 
-    if(data_bit& 8) 
-        D7 = 1;
-    else
-        D7 = 0;
+	if(a & 8)
+		D7 = 1;
+	else
+		D7 = 0;
 }
-
 void Lcd_Cmd(char a)
 {
-    RS = 0;           
-    Lcd_SetBit(a); //Incoming Hex value
-    EN  = 1;         
+	RS = 0;             // => RS = 0
+	Lcd_Port(a);
+	EN  = 1;             // => E = 1
         __delay_ms(4);
-        EN  = 0;         
+        EN  = 0;             // => E = 0
 }
 
 void Lcd_Clear()
 {
-    Lcd_Cmd(0); //Clear the LCD
-    Lcd_Cmd(1); //Move the cursor to first position
+	Lcd_Cmd(0);
+	Lcd_Cmd(1);
 }
 
 void Lcd_Set_Cursor(char a, char b)
 {
-    char temp,z,y;
-    if(a== 1)
-    {
-      temp = 0x80 + b - 1; //80H is used to move the curser
-        z = temp>>4; //Lower 8-bits
-        y = temp & 0x0F; //Upper 8-bits
-        Lcd_Cmd(z); //Set Row
-        Lcd_Cmd(y); //Set Column
-    }
-    else if(a== 2)
-    {
-        temp = 0xC0 + b - 1;
-        z = temp>>4; //Lower 8-bits
-        y = temp & 0x0F; //Upper 8-bits
-        Lcd_Cmd(z); //Set Row
-        Lcd_Cmd(y); //Set Column
-    }
+	char temp,z,y;
+	if(a == 1)
+	{
+	  temp = 0x80 + b - 1;
+		z = temp>>4;
+		y = temp & 0x0F;
+		Lcd_Cmd(z);
+		Lcd_Cmd(y);
+	}
+	else if(a == 2)
+	{
+		temp = 0xC0 + b - 1;
+		z = temp>>4;
+		y = temp & 0x0F;
+		Lcd_Cmd(z);
+		Lcd_Cmd(y);
+	}
 }
 
-void Lcd_Start()
+void Lcd_Init()
 {
-  Lcd_SetBit(0x00);
-  for(int i=1065244; i<=0; i--)  //NOP();  
+  Lcd_Port(0x00);
+   __delay_ms(20);
   Lcd_Cmd(0x03);
-    __delay_ms(5);
+	__delay_ms(5);
   Lcd_Cmd(0x03);
-    __delay_ms(11);
-  Lcd_Cmd(0x03); 
-  Lcd_Cmd(0x02); //02H is used for Return home -> Clears the RAM and initializes the LCD
-  Lcd_Cmd(0x02); //02H is used for Return home -> Clears the RAM and initializes the LCD
-  Lcd_Cmd(0x08); //Select Row 1
-  Lcd_Cmd(0x00); //Clear Row 1 Display
-  Lcd_Cmd(0x0C); //Select Row 2
-  Lcd_Cmd(0x00); //Clear Row 2 Display
+	__delay_ms(11);
+  Lcd_Cmd(0x03);
+  /////////////////////////////////////////////////////
+  Lcd_Cmd(0x02);
+  Lcd_Cmd(0x02);
+  Lcd_Cmd(0x08);
+  Lcd_Cmd(0x00);
+  Lcd_Cmd(0x0C);
+  Lcd_Cmd(0x00);
   Lcd_Cmd(0x06);
 }
 
-void Lcd_Print_Char(char data)  //Send 8-bits through 4-bit mode
+void Lcd_Write_Char(char a)
 {
-   char Lower_Nibble,Upper_Nibble;
-   Lower_Nibble = data&0x0F;
-   Upper_Nibble = data&0xF0;
-   RS = 1;             // => RS = 1
-   Lcd_SetBit(Upper_Nibble>>4);             //Send upper half by shifting by 4
+   char temp,y;
+   temp = a&0x0F;
+   y = a&0xF0;
+   RS = 1;             
+   Lcd_Port(y>>4);             
    EN = 1;
-   for(int i=2130483; i<=0; i--)  //NOP(); 
+   __delay_us(40);
    EN = 0;
-   Lcd_SetBit(Lower_Nibble); //Send Lower half
+   Lcd_Port(temp);
    EN = 1;
-   for(int i=2130483; i<=0; i--)  //NOP();
+   __delay_us(40);
    EN = 0;
 }
 
-void Lcd_Print_String(char *a)
+void Lcd_Write_String(char *a)
 {
-    int i;
-    for(i=0;a[i]!='\0';i++)
-       Lcd_Print_Char(a[i]);  //Split the string using pointers and call the Char function 
+	int i;
+	for(i=0;a[i]!='\0';i++)
+	   Lcd_Write_Char(a[i]);
 }
-//***End of LCD functions***//
+
+
+void system_init (void); // This function will initialise the ports.
+void full_drive (char direction); // This function will drive the motor in full drive mode
+void half_drive (char direction); // This function will drive the motor in full drive mode
+void wave_drive (char direction); // This function will drive the motor in full drive mode
+void ms_delay(unsigned int val);
+void ms_delay(unsigned int val)
+{
+     unsigned int i,j;
+        for(i=0;i<val;i++)
+            for(j=0;j<1650;j++);
+}
 void Initialize_ESP8266(void)
 {
     //****Setting I/O pins for UART****//
@@ -184,17 +165,6 @@ void Initialize_ESP8266(void)
     //__8-bit mode selected__//     
 }
 //________UART module Initialized__________//
-
-//**Function to configure soft_AP**//
-unsigned char esp8266_config_softAP(unsigned char* softssid, unsigned char* softpass) {
-    _esp8266_print("AT+CWSAP=\"");
-    _esp8266_print(softssid);
-    _esp8266_print("\",\"");
-    _esp8266_print(softpass);
-    _esp8266_print("\",5,3\r\n");
-    return _esp8266_waitResponse();
-}
- 
 //**Function to send one byte of date to UART**//
 void _esp8266_putch(unsigned char bt)  
 {
@@ -234,366 +204,92 @@ void ESP8266_send_string(char* st_pt)
 
 
 
-/**
- * Check if the module is started
- *
- * This sends the `AT` command to the ESP and waits until it gets a response.
- *
- * @return true if the module is started, false if something went wrong
- */
-char esp8266_isStarted(void) {
-    _esp8266_print("AT\r\n");
-    return (_esp8266_waitResponse() == ESP8266_OK);
-}
 
-/**
- * Restart the module
- *
- * This sends the `AT+RST` command to the ESP and waits until there is a
- * response.
- *
- * @return true iff the module restarted properly
- */
-char esp8266_restart(void) {
-    _esp8266_print("AT+RST\r\n");
-    if (_esp8266_waitResponse() != ESP8266_OK) {
-        return false;
-    }
-    return (_esp8266_waitResponse() == ESP8266_READY);
-}
 
-/**
- * Enable / disable command echoing.
- *
- * Enabling this is useful for debugging: one could sniff the TX line from the
- * ESP8266 with his computer and thus receive both commands and responses.
- *
- * This sends the ATE command to the ESP module.
- *
- * @param echo whether to enable command echoing or not
- */
-void esp8266_echoCmds(bool echo) {
-    _esp8266_print("ATE");
-    if (echo) {
-        _esp8266_putch('1');
-    } else {
-        _esp8266_putch('0');
-    }
-    _esp8266_print("\r\n");
-    _esp8266_waitFor("OK");
-}
 
-/**
- * Set the WiFi mode.
- *
- * ESP8266_STATION : Station mode
- * ESP8266_SOFTAP : Access point mode
- *
- * This sends the AT+CWMODE command to the ESP module.
- *
- * @param mode an ORed bitmask of ESP8266_STATION and ESP8266_SOFTAP
- */
-void esp8266_mode(unsigned char mode) {
-    _esp8266_print("AT+CWMODE=");
-    _esp8266_putch(mode + '0');
-    _esp8266_print("\r\n");
-    _esp8266_waitResponse();
-}
 
-/**
- * Connect to an access point.
- *
- * This sends the AT+CWJAP command to the ESP module.
- *
- * @param ssid The SSID to connect to
- * @param pass The password of the network
- * @return an ESP status code, normally either ESP8266_OK or ESP8266_FAIL
- */
-unsigned char esp8266_connect(unsigned char* ssid, unsigned char* pass) {
-    _esp8266_print("AT+CWJAP=\"");
-    _esp8266_print(ssid);
-    _esp8266_print("\",\"");
-    _esp8266_print(pass);
-    _esp8266_print("\"\r\n");
-    return _esp8266_waitResponse();
-}
 
-/**
- * Disconnect from the access point.
- *
- * This sends the AT+CWQAP command to the ESP module.
- */
-void esp8266_disconnect(void) {
-    _esp8266_print("AT+CWQAP\r\n");
-    _esp8266_waitFor("OK");
-}
 
-/**
- * Store the current local IPv4 address.
- *
- * This sends the AT+CIFSR command to the ESP module.
- *
- * The result will not be stored as a string but byte by byte. For example, for
- * the IP 192.168.0.1, the value of store_in will be: {0xc0, 0xa8, 0x00, 0x01}.
- *
- * @param store_in a pointer to an array of the type unsigned char[4]; this
- * array will be filled with the local IP.
- */
-void esp8266_ip(unsigned char* store_in) {
-    _esp8266_print("AT+CIFSR\r\n");
-    unsigned char received;
-    do {
-        received = _esp8266_getch();
-    } while (received < '0' || received > '9');
-    for (unsigned char i = 0; i < 4; i++) {
-        store_in[i] = 0;
-        do {
-            store_in[i] = 10 * store_in[i] + received - '0';
-            received = _esp8266_getch();
-        } while (received >= '0' && received <= '9');
-        received = _esp8266_getch();
-    }
-    _esp8266_waitFor("OK");
-}
 
-/**
- * Open a TCP or UDP connection.
- *
- * This sends the AT+CIPSTART command to the ESP module.
- *
- * @param protocol Either ESP8266_TCP or ESP8266_UDP
- * @param ip The IP or hostname to connect to; as a string
- * @param port The port to connect to
- *
- * @return true iff the connection is opened after this.
- */
-char esp8266_start(unsigned char protocol, char* ip, unsigned char port) {
-    _esp8266_print("AT+CIPSTART=\"");
-    if (protocol == ESP8266_TCP) {
-        _esp8266_print("TCP");
-    } else {
-        _esp8266_print("UDP");
-    }
-    _esp8266_print("\",\"");
-    _esp8266_print(ip);
-    _esp8266_print("\",");
-    unsigned char port_str[5] = "\0\0\0\0";
-    sprintf(port_str, "%u", port);
-    _esp8266_print(port_str);
-    _esp8266_print("\r\n");
-    if (_esp8266_waitResponse() != ESP8266_OK) {
-        return 0;
-    }
-    if (_esp8266_waitResponse() != ESP8266_LINKED) {
-        return 0;
-        
-    }
-    return 1;
-}
 
-// Send data (AT+CIPSEND)
-/**
- * Send data over a connection.
- *
- * This sends the AT+CIPSEND command to the ESP module.
- *
- * @param data The data to send
- *
- * @return true iff the data was sent correctly.
- */
-char esp8266_send(unsigned char* data) {
-    unsigned char length_str[6] = "\0\0\0\0\0";
-    sprintf(length_str, "%u", strlen(data));
-    _esp8266_print("AT+CIPSEND=");
-    _esp8266_print(length_str);
-    _esp8266_print("\r\n");
-    while (_esp8266_getch() != '>');
-    _esp8266_print(data);
-    if (_esp8266_waitResponse() == ESP8266_OK) {
-        return 1;
-    }
-    return 0;
-}
 
-/**
- * Read a string of data that is sent to the ESP8266.
- *
- * This waits for a +IPD line from the module. If more bytes than the maximum
- * are received, the remaining bytes will be discarded.
- *
- * @param store_in a pointer to a character array to store the data in
- * @param max_length maximum amount of bytes to read in
- * @param discard_headers if set to true, we will skip until the first \r\n\r\n,
- * for HTTP this means skipping the headers.
- */
-void esp8266_receive(unsigned char* store_in, uint16_t max_length, bool discard_headers) {
-    _esp8266_waitFor("+IPD,");
-    uint16_t length = 0;
-    unsigned char received = _esp8266_getch();
-    do {
-        length = length * 10 + received - '0';
-        received = _esp8266_getch();
-    } while (received >= '0' && received <= '9');
 
-    if (discard_headers) {
-        length -= _esp8266_waitFor("\r\n\r\n");
-    }
 
-    if (length < max_length) {
-        max_length = length;
-    }
 
-    /*sprintf(store_in, "%u,%u:%c%c", length, max_length, _esp8266_getch(), _esp8266_getch());
-    return;*/
 
-    uint16_t i;
-    for (i = 0; i < max_length; i++) {
-        store_in[i] = _esp8266_getch();
-    }
-    store_in[i] = 0;
-    for (; i < length; i++) {
-        _esp8266_getch();
-    }
-    _esp8266_waitFor("OK");
-}
 
-/**
- * Output a string to the ESP module.
- *
- * This is a function for internal use only.
- *
- * @param ptr A pointer to the string to send.
- */
-void _esp8266_print(unsigned const char *ptr) {
-    while (*ptr != 0) {
-        _esp8266_putch(*ptr++);
-    }
-}
 
-/**
- * Wait until we found a string on the input.
- *
- * Careful: this will read everything until that string (even if it's never
- * found). You may lose important data.
- *
- * @param string
- *
- * @return the number of characters read
- */
-inline uint16_t _esp8266_waitFor(unsigned char *string) {
-    unsigned char so_far = 0;
-    unsigned char received;
-    uint16_t counter = 0;
-    do {
-        received = _esp8266_getch();
-        counter++;
-        if (received == string[so_far]) {
-            so_far++;
-        } else {
-            so_far = 0;
-        }
-    } while (string[so_far] != 0);
-    return counter;
-}
 
-/**
- * Wait until we received the ESP is done and sends its response.
- *
- * This is a function for internal use only.
- *
- * Currently the following responses are implemented:
- *  * OK
- *  * ready
- *  * FAIL
- *  * no change
- *  * Linked
- *  * Unlink
- *
- * Not implemented yet:
- *  * DNS fail (or something like that)
- *
- * @return a constant from esp8266.h describing the status response.
- */
-inline unsigned char _esp8266_waitResponse(void) {
-    unsigned char so_far[6] = {0,0,0,0,0,0};
-    unsigned const char lengths[6] = {2,5,4,9,6,6};
-    unsigned const char* strings[6] = {"OK", "ready", "FAIL", "no change", "Linked", "Unlink"};
-    unsigned const char responses[6] = {ESP8266_OK, ESP8266_READY, ESP8266_FAIL, ESP8266_NOCHANGE, ESP8266_LINKED, ESP8266_UNLINK};
-    unsigned char received;
-    unsigned char response;
-    bool continue_loop = true;
-    while (continue_loop) {
-        received = _esp8266_getch();
-        for (unsigned char i = 0; i < 6; i++) {
-            if (strings[i][so_far[i]] == received) {
-                so_far[i]++;
-                if (so_far[i] == lengths[i]) {
-                    response = responses[i];
-                    continue_loop = false;
-                }
-            } else {
-                so_far[i] = 0;
-            }
-        }
-    }
-    return response;
-}
-void main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//deleted code 
+  void Lcd_Shift_Right()
 {
-    //TRISD = 0x00; 
-    PORTB = 0X00; //port B as output
-    //led blinking  start
-    TRISB0 = 0; //RB0 as Output PIN
- 
-    RB0 = 1;  // LED ON
-   
-   
-  
-    //led blinking end
-   // Lcd_Start();
-    Initialize_ESP8266(); //initialize ESP module for UART
-    char set_client="AT+CWMODE=1"; //set ESP as client
-    ESP8266_send_string(set_client);//send data to ESP over UART
-    char set_info="AT+CWJAP="+ssid+password;//connecting to wifi
-    /*__________________ESP-01 setup complete________________________*/
-    char cmd = "AT+CIPSTART=""TCP"",""184.106.153.149"",""80""";//sending IP address of thingspeak
-    char getStr;
-    while(1)
-    {
-        if(IR==1) //checking the value of IR sensor for HIGH output 
-        {
-            char value1 = '5';//setting flag
-    ESP8266_send_string(cmd);//sending the IP address to connect to thingspeak server
-    getStr = "GET /update?api_key="+apiKey; //updating the value using API write key
-    getStr = "&field1=" + getStr + value1;//updating the value using API write key
-    __delay_ms(20000);
-            __delay_ms(200000);//thingspeak updating during this time and ensuring the obstacle is still in the same position.
-    
-        }
-        if(IR==0)//checking the value of IR sensor for LOW output
-        {
-            char value2 = '1';//setting flag
-    ESP8266_send_string(cmd);//sending the IP address to connect to thingspeak server
-    getStr = "GET /update?api_key="+apiKey;//updating the value using API write key
-    getStr = "&field1=" + getStr + value2;//updating the value using API write key
-            __delay_ms(200000);//thingspeak updating during this time and ensuring the obstacle is still in the same position. 
-        }
-    }
-    
-    //Lcd_Set_Cursor(1,1);
-    //Lcd_Print_String("IOT BASED CAR");
-    //Lcd_Set_Cursor(2,1);
-    //Lcd_Print_String("PARKING SYSTEM");
-   //__delay_ms(1500);
-    //Lcd_Clear();
-   
-    
-    
-            
-    while(1)
-    {
-        //do nothing 
-    }
-  
-
+	Lcd_Cmd(0x01);
+	Lcd_Cmd(0x0C);
 }
+
+void Lcd_Shift_Left()
+{
+	Lcd_Cmd(0x01);
+	Lcd_Cmd(0x08);
+}
+
